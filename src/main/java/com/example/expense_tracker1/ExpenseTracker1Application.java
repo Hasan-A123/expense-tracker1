5edcd1feaf6a8cec0ef17f0fb761cb3dcf2dcef2
Hasan-A123 <hasanashraf13@gmail.com>
2026-04-28 16:45:55 +0100

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/expense_tracker1/ExpenseTracker1Application.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracker1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTracker1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expense_tracker1/ExpenseTracker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expense_tracker1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTracker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
